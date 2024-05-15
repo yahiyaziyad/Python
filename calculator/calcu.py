05a2b92702adfb56f879fc08606c0fecdda1a624
yahiyaziyad <yahiyaziyad2628@gmail.com>
2024-05-15 11:06:39 +0530

    calculator

---
+++ calculator/calcu.py
@@ -0,0 +1,34 @@
+print("1.ADD")
+print("2.SUBTRACT")
+print("3.MULTIPLY")
+print("4.DIVIDE")
+print("Pick Operation To Perform :")
+
+operation = int(input())
+
+if operation == 1:
+
+    num1 = input("Enter first number : ")
+    num2 = input("Enter second number : ")
+    print("The Sum is = "+str(int(num1)+int(num2) ))
+
+elif operation == 2:
+
+    num1 = input("Enter first number : ")
+    num2 = input("Enter second number : ")
+    print("The Diff is = "+ str(int(num1)-int(num2)))
+
+elif operation == 3:
+
+    num1 = input("Enter first number : ")
+    num2 = input("Enter second number : ")
+    print("The Product is = "+ str(int(num1)*int(num2)))
+
+elif operation == 4:
+
+    num1 = input("Enter first number : ")
+    num2 = input("Enter second number : ")
+    print("The Quotient is = "+str(int(num1)/int(num2)))
+
+else:
+    print("##invalid##")
---
+++ if.py
@@ -0,0 +1,7 @@
+x=int(input())
+if x<5:
+    print("hi")
+elif x==5:
+    print("poda")
+else :
+    print("hello")
